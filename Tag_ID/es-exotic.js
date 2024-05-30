0adcc702c9dd47b99e9caa8ceec3844b28b43bb0
𖥔 ࣪˖🍁「𝗠𝘂𝗹𝘁𝗶𝗕𝗼𝘁┃ᵒᶠᶜ-ᵃᶜᵒᵘⁿᵗ」🍁𖥔 ࣪˖ <alanluliovargas@gmail.com>
2024-05-30 00:32:23 -0300

    Add files via upload

---
+++ Tag_ID/es-exotic.js
@@ -0,0 +1,73 @@
+const ExoticoLang = () => { return 'es' } 
+//CODIGO DE GATA BOT MD
+//Tienda
+const eExp = () => { return '⚡ Experiencia' } 
+const eDiamante = () => { return '💎 Diamante' } 
+const eDiamantePlus = () => { return '💎+ Diamante+' }
+const eToken = () => { return '🪙 Token' } 
+const eEsmeralda = () => { return '💚 Esmeralda' } 
+const eJoya = () => { return '♦️ Joya' }
+const eMagia = () => { return '🌀 Magia' } 
+const eOro = () => { return '👑 Oro' } 
+const eBotCoins = () => { return '🐱 GataCoins' }
+const eBotTickers = () => { return '🎫 Gata Tickers' } 
+const eEnergia = () => { return '✨ Energía' }
+const ePocion = () => { return '🥤 Poción' }
+const eAgua = () => { return '💧 Agua' }
+const eBasura = () => { return '🗑 Basura' }
+const eMadera = () => { return '🪵 Madera' }
+const eRoca = () => { return '🪨 Roca' }
+const ePiedra = () => { return '🥌 Piedra' }
+const eCuerda = () => { return '🕸️ Cuerda' }
+const eHierro = () => { return '⛓️ Hierro' }
+const eCarbon = () => { return '⚱️ Carbón' }
+const eBotella = () => { return '🍶 Botella' }
+const eLata = () => { return '🥫 Lata' }
+const eCarton = () => { return '🪧 Cartón' } 
+const eEletric = () => { return '💡 Electricidad' }
+const eBarraOro = () => { return '〽️ Barra de Oro' }
+const eOroComun = () => { return '🧭 Oro Común' }
+const eZorroG = () => { return '🦊🌫️ Zorro Grande' }
+const eBasuraG = () => { return '🗑🌫️ Super Basura' }
+const eLoboG = () => { return '🐺🌫️ Super Lobo' }
+const eMaderaG = () => { return '🛷🌫️ Super Madera' }
+const eEspada = () => { return '⚔️ Espada' }
+const eCarnada = () => { return '🪱 Carnada' }
+const eBillete = () => { return '💵 Billetes' }
+const ePinata = () => { return '🪅 Piñata' }
+const eGancho = () => { return '🪝 Gancho' }
+const eCanaPescar = () => { return '🎣 Caña de Pescar' } 
+const eCComun = () => { return '📦 Caja Común' }
+const ePComun = () => { return '🥡 Caja Poco Común' }
+const eCMistica = () => { return '🗳️ Caja Mítica' }
+const eCMascota = () => { return '📫 Caja de Mascotas' }
+const eCJardineria = () => { return '💐 Caja de Jardinería' }
+const eClegendaria = () => { return '🎁 Caja Legendaria' } 
+const eUva = () => { return '🍇 Uva' }
+const eManzana = () => { return '🍎 Manzana' }
+const eNaranja = () => { return '🍊 Naranja' }
+const eMango = () => { return '🥭 Mango' }
+const ePlatano = () => { return '🍌 Platano' } 
+const eSUva = () => { return '🌾🍇 Semillas de uva' }
+const eSManzana = () => { return '🌾🍎 Semillas de manzana' }
+const eSNaranja = () => { return '🌾🍊 Semillas de naranja' }
+const eSMango = () => { return '🌾🥭 Semillas de Mango' }
+const eSPlatano = () => { return '🌾🍌 Semillas de plátano' } 
+const eCentauro = () => { return '🐐 Centauro' }
+const eAve = () => { return '🦅 Ave' }
+const eGato = () => { return '🐈 Gato' }
+const eDragon = () => { return '🐉 Dragón' }
+const eZorro = () => { return '🦊 Zorro' }
+const eCaballo = () => { return '🐎 Caballo' }
+const eFenix = () => { return '🕊️ Fénix' }
+const eLobo = () => { return '🐺 Lobo' }
+const ePerro = () => { return '🐶 Perro' } 
+const eAMascots = () => { return '🍖 Alimento para Mascota' }
+const eCCentauro = () => { return '🐐🥩 Comida de Centauro' }
+const eCAve = () => { return '🦅🥩 Comida de Ave' }
+const eCMagica = () => { return '🌀🥩 Comida Mágica' }
+const eCDragon = () => { return '🐉🥩 Comida de Dragón' }
+const eACaballo = () => { return '🐎🥩 Alimentos Para Caballo' }
+const eCFenix = () => { return '🕊️🥩 Comida de Fénix' } 
+
+export default { ExoticoLang, eBarraOro, ePocion, eExp, eDiamante, eDiamantePlus, eEsmeralda, eToken, eMagia, eJoya, eOro, eBotCoins, eBotTickers, eEnergia, eMadera, eBasura, eAgua, ePiedra, eRoca, eCuerda, eHierro, eEletric, eCarton, eLata, eBotella, eCarbon,  eCFenix, eCCaballo, eCDragon, eCMagia, eCAve, eCCentauro, eAMascots, ePerro, eLobo, eFenix, eCaballo, eZorro, eDragon, eGato, eAve, eCentauro, eSPlatano, eSMango, eSNaranja, eManzana, eUva, eClegendaria, eCJardineria, eCMascota, eCMistica, ePComun, eCComun, eCanaPescar, eGancho, ePinata, eCarnada, eEspada, eLoboG, eZorroG, eMaderaG, eOroComun, eBasuraG, }
