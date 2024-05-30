af5ad09e01135a51691e277008eaad49c6214ffb
𖥔 ࣪˖🍁「𝗠𝘂𝗹𝘁𝗶𝗕𝗼𝘁┃ᵒᶠᶜ-ᵃᶜᵒᵘⁿᵗ」🍁𖥔 ࣪˖ <alanluliovargas@gmail.com>
2024-05-30 02:18:54 -0300

    ⸙🍁🄼𝘂𝗹𝘁𝗶 🅱︎𝗼𝘁┃ᵒᶠᶜ⸙🍂

--- plugins/hot-exotic-endeavor.js
+++ plugins/hot-exotic-endeavor.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 let handler = async(m, { conn, usedPrefix, command }) => {
-let res = (await axios.get(`https://raw.githubusercontent.com/MultiBot-OFC/ExoticoBot-MD/master/node_animeboy_json/hot-endeavor.json`)).data  
+let res = (await axios.get(`https://raw.githubusercontent.com/MultiBot-OFC/ExoticoBot-MD/master/Multi_Imagen/node_animeboy_json/hot-endeavor.json`)).data  
 let url = await res[Math.floor(res.length * Math.random())]
 conn.sendButton(m.chat, "🍁 🔥 𝙀𝙣𝙙𝙚𝙖𝙫𝙤𝙧 🔥 🍁", '𝘏𝘰𝘵 𝘌𝘯𝘥𝘦𝘢𝘷𝘰𝘳', url, [[mssg.botonSiguiente, `${usedPrefix + command}`]], null, null, m)}
 handler.help = ['hot asuma']
