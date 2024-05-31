6506d5908191573b3a7b3242e3f2b7cefab41e90
joelch4ju <164943790+joelch4ju@users.noreply.github.com>
2024-05-30 22:09:17 -0500

    Actualizar fondo-wallpaper.js

--- Multi_Imagen/node_wallpaper.json/fondo-wallpaper.js
+++ Multi_Imagen/node_wallpaper.json/fondo-wallpaper.js
@@ -1,10 +1,10 @@
 [
-  "https://telegra.ph/file/cbe704dec86beed6a88cb.jpg",
   "https://telegra.ph/file/4ccfd31caa26913b043eb.jpg",
   "",
   "",
   "",
-  "https://telegra.ph/file/97ae6e760688eaa7afd98.jpg",
+  "",
+  "",
   "",
   "",
   "",
