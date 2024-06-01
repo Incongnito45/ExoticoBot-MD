d94360e7a2c70ccb5ae953c7e70488d2c18acaf0
𖥔 ࣪˖🍁「𝗠𝘂𝗹𝘁𝗶𝗕𝗼𝘁┃ᵒᶠᶜ-ᵃᶜᵒᵘⁿᵗ」🍁𖥔 ࣪˖ <alanluliovargas@gmail.com>
2024-06-01 02:59:48 -0300

    〘⸙🎉🄴𝘅𝗼𝘁𝗶𝗰🅾︎⸙「👤」🅱︎𝗼🅃⸙💧⸙〙

--- koyeb.js
+++ koyeb.js
@@ -4,7 +4,7 @@ RUN sudo dnf -y update &&\
     sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm &&\
     sudo dnf install -y git ffmpeg ImageMagick nodejs yarnpkg libwebp &&\
     sudo dnf clean all -y
-
+ 
 RUN git clone https://github.com/MultiBot-OFC/ExoticoBot-MD
 
 WORKDIR /root/ExoticoBot-MD
