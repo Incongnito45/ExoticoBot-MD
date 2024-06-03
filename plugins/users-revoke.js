2fd2ec71a30d7c238e1636b770517d7a441c3d3e
Ramón <165967820+RamonFTGD@users.noreply.github.com>
2024-06-02 19:58:45 -0600

    users-revoke.js
    Actualizado por Ramón-MD :D

--- plugins/users-revoke.js
+++ plugins/users-revoke.js
@@ -1,7 +1,22 @@
 export async function before(m,  {conn, participants, isBotAdmin}) {
+    let grupo_completo = participants;
+    let administrador = false;
+    try{
+        for(let u = 0; grupo_completo.length; u++){
+            if(m.sender === grupo_completo[u].id){
+                if(grupo_completo[u].admin == "admin" || grupo_completo[u].admin == "superadmin"){
+                    administrador = true;
+                    break;
+                }else{
+                    break;
+                }
+            }
+        }
+    }catch{
+    }
     if(m.text.toLowerCase() === "reset"){
       if(isBotAdmin){
-        if(m.isAdmin){
+        if(administrador){
           await this.groupInviteCode(m.chat);
           await this.sendMessage(m.chat, {text: "El enlace se ah restablecido con exito"});
         }else{
