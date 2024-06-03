1e1104e401a384fba9cc08e09320005a5cda0a6b
Ramón <165967820+RamonFTGD@users.noreply.github.com>
2024-06-02 18:58:10 -0600

    users-revoke.js

--- plugins/users-revoke.js
+++ plugins/users-revoke.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
   try{
-    let res = await this.groupRevokeInvite(m.chat);
+    let res = await conn.groupRevokeInvite(m.chat);
   }catch{
   }
 conn.sendMessage(m.chat, {text: '᥀·࣭࣪̇˖🪐◗ 𝘼𝙦𝙪𝙞 𝙩𝙞𝙚𝙣𝙚𝙨 𝙚𝙡 𝙣𝙪𝙚𝙫𝙤 𝙚𝙣𝙡𝙖𝙘𝙚 𝙧𝙚𝙨𝙩𝙖𝙗𝙡𝙚𝙘𝙞𝙙𝙤!!!', + '\n\n*https://chat.whatsapp.com/' + res + '*'}, {quoted: fkontak)};
