00d5235880b635a6dbb74c0f290c51dd3afb4993
𖥔 ࣪˖🍁「𝗠𝘂𝗹𝘁𝗶𝗕𝗼𝘁┃ᵒᶠᶜ-ᵃᶜᵒᵘⁿᵗ」🍁𖥔 ࣪˖ <alanluliovargas@gmail.com>
2024-06-05 05:27:43 -0300

    〘⸙🎉🄴𝘅𝗼𝘁𝗶𝗰🅾︎⸙「👤」🅱︎𝗼🅃⸙💧⸙〙

--- handler.js
+++ handler.js
@@ -6,7 +6,7 @@ import path, { join } from 'path'
 import { unwatchFile, watchFile } from 'fs'
 import chalk from 'chalk'   
 import fetch from 'node-fetch'
-import './EXOTICO/conexion/archivos.js'
+import './plugins/archivos.js'
  
 /**
  * @type {import('@adiwajshing/baileys')}  
